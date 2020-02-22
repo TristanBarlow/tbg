015d53552c519dec6fee66dbc83af0f1548c9091
Tristan <tristanbarlowg@gmail.com>
2020-02-22 09:40:51 +0000

    initial commit of react app and some styles

---
+++ tristanbarlowgriffin.co.uk/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/main.css'
+
+function App() {
+  return (
+    <div className="title">
+      <button className="button is-primary">fooo</button>
+      <header className="tile is-parent box">
+      </header>
+    </div>
+  )
+}
+
+export default App
