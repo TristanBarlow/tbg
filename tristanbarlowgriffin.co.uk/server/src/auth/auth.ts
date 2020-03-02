3a2d55df2e761f1dd8fb6ced59be6a4f808e5b60
Tristan Barlow <tristanbarlowg@gmail.com>
2020-03-02 17:24:23 +0000

    removed unecessary types

--- tristanbarlowgriffin.co.uk/server/src/auth/auth.ts
+++ tristanbarlowgriffin.co.uk/server/src/auth/auth.ts
@@ -1,7 +1,7 @@
 import { MANAGE_KEY } from '../key'
 import { RequestHandler } from 'express'
 
-function IsKeyValid (a: any | undefined | null): boolean {
+function IsKeyValid (a: any): boolean {
   if (typeof a !== 'string') return false
   return a === MANAGE_KEY
 }
