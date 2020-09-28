4d8969ff9e11e5befe85e79b9be23f0ee089c3a5
Tristan Barlow <tristanbarlowg@gmail.com>
2020-09-28 19:46:31 +0100

    switched to yarn workspaces. And started function component rewrite

--- tristanbarlowgriffin.co.uk/website/src/pages/Contact.tsx
+++ tristanbarlowgriffin.co.uk/website/src/pages/Contact.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 
-export default class Contact extends React.Component<{}>{
-  render () {
-    return (
-      <div className="tile is-parent">
-        <div className="title"></div>
-        <div className="tile is-child center-a col flex">
-          <p className="title is-1  is-size-4-mobile">Contact</p>
-          <div className="title">
-            <p style={ { maxWidth: '520px' } } className="title is-5 is-size-7-mobile">If you do want to contact me for whatever reason send me an email. I will try to respond as soon as I can :)</p>
-          </div>
-          <p className="title is-4 is-size-6-mobile">tristanbarlowg@gmail.com</p>
+export default function Contact () {
+  return (
+    <div className="tile is-parent">
+      <div className="title"></div>
+      <div className="tile is-child center-a col flex">
+        <p className="title is-1  is-size-4-mobile">Contact</p>
+        <div className="title">
+          <p style={ { maxWidth: '520px' } } className="title is-5 is-size-7-mobile">If you do want to contact me for whatever reason send me an email. I will try to respond as soon as I can :)</p>
         </div>
+        <p className="title is-4 is-size-6-mobile">tristanbarlowg@gmail.com</p>
       </div>
-    )
-  }
+    </div>
+  )
 }
