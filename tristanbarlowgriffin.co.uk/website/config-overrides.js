71e30dd75c9e6dc49910603bceabf56959dbeba0
Tristan Barlow <tristanbarlowg@gmail.com>
2020-09-29 08:28:50 +0100

    fixed the worker loader not working properly

--- tristanbarlowgriffin.co.uk/website/config-overrides.js
+++ tristanbarlowgriffin.co.uk/website/config-overrides.js
@@ -1,9 +1,19 @@
-const { override, babelInclude, a } = require('customize-cra')
+const { override, babelInclude, res } = require('customize-cra')
 const path = require('path')
+
+const workerLoader = {
+  test: /\.worker\.ts$/,
+  include: path.resolve('src'),
+  use: ["workerize-loader", "babel-loader"]
+}
 
 module.exports = override(
   babelInclude([
     path.resolve('src'),        // make sure you link your own source
     path.resolve('../packages/types'),  // your shared module to transpile
   ]),
+  (x => {
+    console.log(x.module.rules[2].oneOf.slice(1, 0, workerLoader))
+    return x
+  })
 )
