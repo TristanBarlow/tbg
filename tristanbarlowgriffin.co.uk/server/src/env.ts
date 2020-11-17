e217da39592801fb4f119e7e66ee86814df84c7b
Tristan Barlow <tristanbarlowg@gmail.com>
2020-11-17 20:41:34 +0000

    added support for key to be substituted

--- tristanbarlowgriffin.co.uk/server/src/env.ts
+++ tristanbarlowgriffin.co.uk/server/src/env.ts
@@ -3,17 +3,18 @@ import { resolve } from 'path'
 export interface Env {
   PORT: string
   MODE: 'dev' | 'prod'
+  KEY: string
 }
 
-const env: Env = {
+export const CONFIG: Env = {
   PORT: process.env.PORT || '8080',
-  MODE: process.env.MODE !== 'prod' ? 'dev' : 'prod'
+  MODE: process.env.MODE !== 'prod' ? 'dev' : 'prod',
+  KEY: process.env.KEY || ''
 }
 
 export function serviceAccountPath (): string | undefined {
-  if (env.MODE === 'dev') return resolve(__dirname, '..', 'image-manager-SA.json')
+  if (CONFIG.MODE === 'dev') return resolve(__dirname, '..', 'image-manager-SA.json')
   return undefined
 }
 
-console.warn('STARTING SERVER WITH ENV: ', env)
-export default env
+console.warn('STARTING SERVER WITH ENV: ', CONFIG)
