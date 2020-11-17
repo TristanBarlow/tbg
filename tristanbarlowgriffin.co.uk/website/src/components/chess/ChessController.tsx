b9b983acdd2a0ad28ad653de0b992afed5cfb922
Tristan Barlow <tristanbarlowg@gmail.com>
2020-11-17 07:30:57 +0000

    converted chess contoller to functional component

--- tristanbarlowgriffin.co.uk/website/src/components/chess/ChessController.tsx
+++ tristanbarlowgriffin.co.uk/website/src/components/chess/ChessController.tsx
@@ -1,106 +1,84 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { ChessPlayer, PlayersTypes, PlayerFactory, PlayerColour, MoveResponse } from '../../ts/chess/players'
 import ChessBoard from './ChessBoard'
 import Button, { Colors } from '../Button'
 import OptionSelecta from '../OptionSelecta'
 import { Flex } from '@chakra-ui/core'
 
-interface State {
-  white: ChessPlayer
-  black: ChessPlayer
-  paused: boolean
-  showControls: boolean
-  whiteLog: string
-  blackLog: string
-}
+const playerOptions = Object.values(PlayersTypes)
 interface Props {
 
 }
-export default class ChessController extends React.Component<Props, State>{
-  undo!: () => void
-  constructor (p: Props) {
-    super(p)
-    this.state = {
-      paused: true,
-      showControls: false,
-      black: PlayerFactory(PlayersTypes.RANDOM),
-      white: PlayerFactory(PlayersTypes.RANDOM),
-      blackLog: '',
-      whiteLog: ''
-    }
-  }
+export default function ChessController () {
+  const [paused, setPauses] = useState(true)
+  const [showControls, setShowControls] = useState(false)
+  const [black, setBlack] = useState(PlayerFactory(PlayersTypes.RANDOM))
+  const [white, setWhite] = useState(PlayerFactory(PlayersTypes.RANDOM))
+  const [blackLog, setBlackLog] = useState<string[]>([])
+  const [whiteLog, setWhiteLog] = useState<string[]>([])
 
-  get controls (): JSX.Element | null {
-    if (!this.state.showControls) return null
-    return (
-      <div>
-        <OptionSelecta<PlayersTypes>
-          label="White"
-          value={ this.state.white.name }
-          options={ Object.values(PlayersTypes) }
-          onChange={ (x) => this.setState({ white: PlayerFactory(x) }) } />
+  const undo = useRef<() => void>()
 
-        <OptionSelecta<PlayersTypes>
-          label="Black"
-          value={ this.state.black.name }
-          options={ Object.values(PlayersTypes) }
-          onChange={ (x) => this.setState({ black: PlayerFactory(x) }) } />
-
-        <Button
-          color={ Colors.INFO }
-          label={ this.state.paused ? 'Start' : 'Pause' }
-          onClick={ () => { this.setState({ paused: !this.state.paused }) } } />
-
-        <Button
-          color={ Colors.INFO }
-          label="UNDO"
-          onClick={ () => {
-            this.setState({ paused: true })
-            this.undo()
-          } } />
-      </div>
-    )
-  }
-
-  get outPutLog (): JSX.Element {
-    return (
-      <div className="column">
-        <div>{ this.state.whiteLog }</div>
-        <div>{ this.state.blackLog }</div>
-      </div>)
-  }
-
-  updateStats (color: PlayerColour, response: MoveResponse) {
+  function updateStats (color: PlayerColour, response: MoveResponse) {
     if (color === 'white') {
-      this.setState({ whiteLog: this.state.whiteLog + (response.rating || '') })
+      setWhiteLog([...whiteLog, response.rating.toString()])
     } else {
-      this.setState({ blackLog: this.state.blackLog + (response.rating || '') })
+      setBlackLog([...blackLog, response.rating.toString()])
     }
   }
 
-  render () {
-    return (
+  const controls = showControls && (
+    <Flex flexDirection="column" w="100%">
+      <OptionSelecta<PlayersTypes>
+        label="White"
+        value={ white.name }
+        options={ playerOptions }
+        onChange={ (x) => setWhite(PlayerFactory(x)) } />
+
+      <OptionSelecta<PlayersTypes>
+        label="Black"
+        value={ black.name }
+        options={ Object.values(PlayersTypes) }
+        onChange={ (x) => setBlack(PlayerFactory(x)) } />
+
+      <Button
+        color={ Colors.INFO }
+        label={ paused ? 'Start' : 'Pause' }
+        onClick={ () => { setPauses(!paused) } } />
+
+      <Button
+        color={ Colors.INFO }
+        label="UNDO"
+        onClick={ () => {
+          setPauses(true)
+          if (undo.current) {
+            undo.current()
+          }
+        } } />
+    </Flex>
+  )
+
+  return < Flex >
+    <Flex flexDirection="row" flexWrap="wrap" justify="center" p={ 1 }>
+      <ChessBoard
+        setUndo={ (x) => undo.current = x }
+        onMove={ updateStats }
+        black={ black }
+        pause={ paused }
+        white={ white } />
       <Flex>
-        <Flex flexDirection="row" flexWrap="wrap" justify="center" p={ 1 }>
-          <ChessBoard
-            setUndo={ (x) => this.undo = x }
-            onMove={ (x, y) => this.updateStats(x, y) }
-            black={ this.state.black }
-            pause={ this.state.paused }
-            white={ this.state.white } />
-          <Flex>
-            <Flex className="column">
-              <Button
-                color={ Colors.INFO }
-                label={ this.state.showControls ? 'Hide' : 'Show' }
-                onClick={ () => { this.setState({ showControls: !this.state.showControls }) } } />
-              { this.controls }
-              { this.updateStats }
-            </Flex>
-            { this.outPutLog }
-          </Flex>
+        <Flex className="column">
+          <Button
+            color={ Colors.INFO }
+            label={ showControls ? 'Hide' : 'Show' }
+            onClick={ () => { setShowControls(!showControls) } } />
+          { controls }
+        </Flex>
+        <Flex flexDirection="column">
+          <div>{ whiteLog }</div>
+          <div>{ blackLog }</div>
         </Flex>
       </Flex>
-    )
-  }
+    </Flex>
+  </Flex >
 }
