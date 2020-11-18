74fa5c3e80e5d6335ba79f9f0a31ba1eca93d63a
Tristan Barlow <tristanbarlowg@gmail.com>
2020-11-18 08:06:25 +0000

    sorting out typechecking not working

--- tristanbarlowgriffin.co.uk/website/config-overrides.js
+++ tristanbarlowgriffin.co.uk/website/config-overrides.js
@@ -1,19 +1,17 @@
-const { override, babelInclude, res } = require('customize-cra')
+const { override, babelInclude, addBabelPlugin } = require('customize-cra')
 const path = require('path')
+const { exit } = require('process')
 
 const workerLoader = {
-  test: /\.worker\.ts$/,
+  test: /\.worker\.ts/,
   include: path.resolve('src'),
   use: ["workerize-loader", "babel-loader"]
 }
 
 module.exports = override(
+  addBabelPlugin(["@babel/plugin-transform-typescript", { "allowNamespaces": true }]),
   babelInclude([
     path.resolve('src'),        // make sure you link your own source
     path.resolve('../packages/types'),  // your shared module to transpile
-  ]),
-  (x => {
-    console.log(x.module.rules[2].oneOf.slice(1, 0, workerLoader))
-    return x
-  })
+  ])
 )
