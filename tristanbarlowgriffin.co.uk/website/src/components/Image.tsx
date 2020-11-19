cf32f95fdf8a53403f7a9e46b73059cb2a45b678
Tristan Barlow <tristanbarlowg@gmail.com>
2020-11-19 23:03:33 +0000

    Fixes and UI improvements for both the admin and project views

--- tristanbarlowgriffin.co.uk/website/src/components/Image.tsx
+++ tristanbarlowgriffin.co.uk/website/src/components/Image.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { ImageMeta, isMeta } from '@tbg/types'
+import { Image, ImageProps } from '@chakra-ui/react'
 
-interface Props {
+interface Props extends ImageProps {
   meta: ImageMeta | string
   loadSize?: number
   width?: string,
   height?: string
 }
-export default function ImageEle ({ meta, width, height }: Props) {
+export default function ImageEle (props: Props) {
+  const { meta, width, height } = props
   const src = `${ process.env.REACT_APP_SERVER_URL }/api/image/${ isMeta(meta) ? meta.name : meta }`
   const description = isMeta(meta) && meta.description
-  return <img
+  return <Image
     alt={ description || 'no description' }
     style={ { width, height, objectFit: 'contain', borderRadius: '3%' } }
     src={ src }
+    { ...props }
   />
 }
--- tristanbarlowgriffin.co.uk/website/src/pages/Manage.tsx
+++ tristanbarlowgriffin.co.uk/website/src/pages/Manage.tsx
@@ -5,6 +5,7 @@ import ProjectManager from '../components/ProjectManager'
 import { Route, Switch, useHistory } from 'react-router'
 import { Link } from 'react-router-dom'
 import { Auth } from '../ts/Auth'
+import { Flex } from '@chakra-ui/react'
 
 export default function Manage () {
   const history = useHistory()
@@ -15,19 +16,17 @@ export default function Manage () {
   }, [history])
 
   return (
-    <div>
+    <Flex flexDirection="column" w="100%">
       <div className="tabs">
         <ul>
           <Link to="/admin/images">Images</Link>
           <Link to="/admin/projects">Projects</Link>
         </ul>
       </div>
-      <div>
-        <Switch>
-          <Route path="/admin/images"><ImageManager /></Route>
-          <Route><ProjectManager /></Route>
-        </Switch>
-      </div>
-    </div>
+      <Switch>
+        <Route path="/admin/images"><ImageManager /></Route>
+        <Route><ProjectManager /></Route>
+      </Switch>
+    </Flex>
   )
 }
