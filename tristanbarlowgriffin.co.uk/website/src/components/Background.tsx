b243ebbdf2ea8d32ad12d0b106a8f183448af9e9
Tristan <tristanbarlowg@gmail.com>
2020-11-27 20:28:56 +0000

    fixed anonymouse default function linting error

--- tristanbarlowgriffin.co.uk/website/src/components/Background.tsx
+++ tristanbarlowgriffin.co.uk/website/src/components/Background.tsx
@@ -4,7 +4,7 @@ interface Props {
   color: string
 }
 
-export default function (c: Props) {
+export default function Background (c: Props) {
   return (
     <div style={ { overflow: '0', backgroundColor: c.color, width: '100vw', height: '100vh', position: 'fixed', left: 0, top: 0, zIndex: -1 } }></div>
   )
