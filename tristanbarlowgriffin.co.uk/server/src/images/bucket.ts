f7bae7a592ccad5f2404c28b41583bb375a96ffe
Tristan <tristanbarlowg@gmail.com>
2020-11-27 21:19:40 +0000

    added sharp and started resize work

--- tristanbarlowgriffin.co.uk/server/src/images/bucket.ts
+++ tristanbarlowgriffin.co.uk/server/src/images/bucket.ts
@@ -1,10 +1,11 @@
 import { storage } from '../common/storage'
 import { Readable, Writable } from 'stream'
 
-export const bucket = storage().bucket('tbg-assets')
+export const rawBucket = storage().bucket('tbg-assets')
+export const publicBucket = storage().bucket('tbg-assets-public')
 
 export async function getReadStream (id: string): Promise<Readable> {
-  const [file] = await bucket.file(id).get()
+  const [file] = await rawBucket.file(id).get()
   if (!file.exists())
     throw Error(`File: ${ id } does not exist`)
 
@@ -12,11 +13,11 @@ export async function getReadStream (id: string): Promise<Readable> {
 }
 
 export function createWriteStream (id: string, contentType: string): Writable {
-  return bucket.file(id).createWriteStream({ contentType, resumable: false, metadata: { uploadTime: Date.now() / 1000 } })
+  return rawBucket.file(id).createWriteStream({ contentType, resumable: false, metadata: { uploadTime: Date.now() / 1000 } })
 }
 
 export async function deleteImage (id: string): Promise<void> {
-  await bucket.file(id).delete()
+  await rawBucket.file(id).delete()
 }
 
 export function asyncPipe (w: Writable, r: Readable): Promise<boolean> {
