151fe3fa11f91587831f9ef1b4a9d20b8fd0d4a3
Tristan Barlow <tristanbarlowg@gmail.com>
2021-05-05 21:08:42 +0100

    Next Js nav bar

--- tbg/components/header/index.tsx
+++ tbg/components/header/index.tsx
@@ -1,11 +1,14 @@
 import { useIsMobile } from 'common/responsive'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import styles from './index.module.css'
 
 export default function Header() {
   const [isActive, setIsActive] = useState(false)
   const isMobile = useIsMobile()
+
+
+  const close = useCallback(()=> setIsActive(false), [])
 
   const burger = isMobile && (
     <button 
@@ -17,19 +20,28 @@ export default function Header() {
       </span>
     </button>
   ) 
-        
-  const menuClass = isMobile && styles.menuActive
+  
+  const menuEle =  <menu className={styles.myMenu}>
+    <Link href="/projects">Projects</Link>      
+    <Link href="/chess">Chess</Link>      
+    <Link href="/contact">Contact</Link>      
+  </menu>
+
+  const asideClass = isActive && styles.navBarAsideActive
+  const aside = isMobile && <aside onClick={close} className={`${styles.navBarAside} ${asideClass}`}>
+    {isMobile && menuEle}
+  </aside>
+
   return (
-    <header className={`${styles.navBar} shadow1`}>
-      <Link href="/">
-        <a className={`bold pointer ${styles.tbg}`}>TBG</a>
-      </Link>
-      {burger}
-      <menu>
-        <Link href="/projects">Projects</Link>      
-        <Link href="/chess">Chess</Link>      
-        <Link href="/contact">Contact</Link>      
-      </menu>
-    </header>
+    <>
+      <header className={`${styles.navBar} shadow1`}>
+        <Link href="/">
+          <a className={`bold pointer ${styles.tbg}`}>TBG</a>
+        </Link>
+        {!isMobile && menuEle}
+        {burger}
+      </header>
+      {aside}
+    </>
   )
 }
--- tbg/pages/index.tsx
+++ tbg/pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
         <p className={styles.name}>Tristan Barlow-Griffin</p>
         <p>Full Stack Software Engineer</p>
         <p>Cornwall, UK</p>
-        <p style={{width:10}}>UK</p>
         <section className={styles.icons}>
           <a href="https://linkedin.com/in/tristan-barlow-griffin-2b4a4817a/" rel="noopener noreferrer" target="_blank" className="selectable p-x-md">
             <Image alt="Link to linkin"  src="/linked-in.png" width={70} height={70} />
