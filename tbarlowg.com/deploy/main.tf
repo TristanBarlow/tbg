908f76a344e338fa3042a78d11b00d0f69dff784
Tristan Barlow <tristanbarlowg@gmail.com>
2025-05-29 21:49:09 +0100

    Upgrades - Setup terraform

---
+++ tbarlowg.com/deploy/main.tf
@@ -0,0 +1,46 @@
+variable "gcp_project_id" {
+  type = string
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = "europe-west1"
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  name                = "tbarlowg"
+  location            = "europe-west1"
+  deletion_protection = false
+  ingress             = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "gcr.io/${var.gcp_project_id}/tbg"
+      resources {
+        limits = {
+          cpu    = "1"
+          memory = "2Gi"
+        }
+      }
+    }
+  }
+
+}
+
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_v2_service.default.location
+  project  = google_cloud_run_v2_service.default.project
+  service  = google_cloud_run_v2_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
