cb38918f4fb80dacdc7d85ae4d11feb7dfb5cb84
Tristan Barlow <tristanbarlowg@gmail.com>
2025-08-01 20:50:31 +0100

    Cloudflare - removed API folder (for now)

--- tbarlowg.com/api/src/common/firestore.ts
+++
@@ -1,5 +0,0 @@
-import { Firestore } from '@google-cloud/firestore'
-import { getCredentials } from '../env'
-
-let _fs: Firestore | null = null
-export const firestore = (): Firestore => _fs ? _fs : _fs = new Firestore(getCredentials())
